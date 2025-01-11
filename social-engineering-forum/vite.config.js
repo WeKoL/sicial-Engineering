e8f2c514af908856537aa6dceb6bb6008eb14b1e
Zachar <WeKoLzoha@gmail.com>
2025-01-11 12:15:09 +0400

    new commit

--- social-engineering-forum/vite.config.js
+++ social-engineering-forum/vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import chokidar from 'chokidar';
+ import chokidar from 'chokidar';
 
 export default defineConfig({
- plugins: [
-     laravel({
-         input: [
-           'resources/scss/app.scss',
-           'resources/js/app.js'
-         ],
-         refresh: true,
-     }),
- ],
-  server: {
+    plugins: [
+        laravel({
+            input: ['resources\scss\app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+   server: {
       watch: {
         usePolling: true,
       }
